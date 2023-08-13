baac721a32dcc62ae438fcde277993b9a596380f
LBF38 <contact.lbf38@gmail.com>
2023-08-13 15:06:03 +0200

    refactor(gendoc): :hammer: add modified api-documenter

--- config/gendoc.sh
+++ config/gendoc.sh
@@ -9,14 +9,25 @@ api-extractor run --local --verbose
 # Remove everything under ../en/Reference/TypeScript API before doing step 2
 echo "current directory : $(pwd)"
 echo "Removing old files in ../../en/Reference/TypeScript API..."
-rm -rf ../en/Reference/TypeScript\ API/*
-mkdir -p ../en/Reference/TypeScript\ API
+rm -rf ../../en/Reference/TypeScript\ API/*
+mkdir -p ../../en/Reference/TypeScript\ API
 
 # 2. Run api-documenter and ensure it finishes before proceeding
 # The absolute path is not a bug; this is Silver's fork of api-documenter
 # Ths fork is available at here if you want to bulid your own: https://github.com/ericaxu/rushstack-obsidian
+echo "Installing the forked api-documenter..."
+if ! (node --version | grep -q "v16"); then
+	echo "Use NodeJS 16 or above"
+	exit 1
+fi
+git clone https://github.com/ericaxu/rushstack-obsidian.git rushstack-obsidian
+npm install -g @microsoft/rush
+cd ./rushstack-obsidian || exit
+rush update --bypass-policy
+rush build
+cd ..
 echo "Running api-documenter..."
-api-documenter markdown --output ../../en/Reference/TypeScript\ API
+./rushstack-obsidian/apps/api-documenter/bin/api-documenter markdown --output ../../en/Reference/TypeScript\ API
 
 # 3. Run post-process.js with Node.js
 echo "Moving to config folder..."
