4a0885f84eb987700597716cfd148f6c1c37ae78
LBF38 <contact.lbf38@gmail.com>
2023-08-13 14:01:51 +0200

    refactor(gendoc): :truck: update gendoc script
    - move api-extractor.json to obsidian-api/api-extractor.json.
    => it is in the git submodule now.
    
    - update the gendoc.sh script accordingly

--- config/gendoc.sh
+++ config/gendoc.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
 # 1. Run api-extractor
+echo "Moving to obsidian-api folder..."
+cd obsidian-api || exit # git submodule folder w/ all obsidian APIs types.
+echo "Running api-extractor..."
 api-extractor run --local --verbose
 
 # Remove everything under ../en/Reference/TypeScript API before doing step 2
+echo "current directory : $(pwd)"
+echo "Removing old files in ../../en/Reference/TypeScript API..."
 rm -rf ../en/Reference/TypeScript\ API/*
 mkdir -p ../en/Reference/TypeScript\ API
 
 # 2. Run api-documenter and ensure it finishes before proceeding
 # The absolute path is not a bug; this is Silver's fork of api-documenter
 # Ths fork is available at here if you want to bulid your own: https://github.com/ericaxu/rushstack-obsidian
-api-documenter markdown --output ../en/Reference/TypeScript\ API && \
+echo "Running api-documenter..."
+api-documenter markdown --output ../../en/Reference/TypeScript\ API
 
 # 3. Run post-process.js with Node.js
+echo "Moving to config folder..."
+cd ..
+echo "Running post-process.js..."
 node post-process.js
